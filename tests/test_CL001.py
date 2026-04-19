34a48124cee1a200b021ea6f0329ab3bc52c6d3c
Bulat <bulatsharipov240@gmail.com>
2026-04-19 18:13:07 +0300

    test: make full test coverage for CL001

--- tests/test_CL001.py
+++ tests/test_CL001.py
@@ -2,6 +2,50 @@ from src import main
 from src.rules.CL001 import Rule
 
 
-def test_exec_with_rules_smoke() -> None:
-    r = main.exec_with_rules("select 1", [Rule])
-    assert not r[0].triggered
+def _assert_triggered(query: str) -> None:
+    diagnostics = main.exec_with_rules(query, [Rule])
+    assert diagnostics[0].triggered
+
+
+def _assert_not_triggered(query: str) -> None:
+    diagnostics = main.exec_with_rules(query, [Rule])
+    assert not diagnostics[0].triggered
+
+
+def test_not_triggered_for_single_literal() -> None:
+    _assert_not_triggered("select 1")
+
+
+def test_duplicate_ints() -> None:
+    _assert_triggered("select 1, 1")
+
+
+def test_duplicate_strings() -> None:
+    _assert_not_triggered("""
+    select id
+    , status = 'new' as is_finished
+    from orders
+    """)
+
+    _assert_triggered("""
+    select id
+    , status = 'new' as is_finished
+    , status = 'new' and price > 100 as is_high_priority
+    from orders
+    """)
+
+
+def test_duplicate_tuples() -> None:
+
+    _assert_not_triggered("""
+    select id
+    , status in ('new', 'pending') as is_finished
+    from orders
+    """)
+
+    _assert_triggered("""
+    select id
+    , status in ('new', 'pending') as is_finished
+    , status in ('new', 'pending') and price > 100 as is_high_priority
+    from orders
+    """)
